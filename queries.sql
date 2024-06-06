1e30a2d84156f27f2c8d146e0d6ef9cd0423b1d2
DAVolgas <volgas1974@gmail.com>
2024-06-06 15:22:38 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -2,8 +2,8 @@
 select COUNT(customer_id) as customers_count from customers;
 
 -- отчет о десятке лучших продавцов 
-select 
-	e.first_name ||' '|| e.last_name as seller,
+select
+	e.first_name||' '||e.last_name as seller,
 	COUNT(s.sales_id) as operations,
 	floor(SUM(s.quantity * p.price)) as income
 from employees e 
@@ -11,7 +11,7 @@ inner join sales s
 	on s.sales_person_id = e.employee_id 
 inner join products p 
 	on p.product_id = s.product_id 
-group by e.first_name ||' '|| e.last_name
+group by e.first_name||' '||e.last_name
 order by floor(SUM(s.quantity * p.price)) desc
 limit 10;
 
@@ -26,14 +26,14 @@ with total_avg_amount as
 )
 -- отчет о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
 select 
-	e.first_name ||' '|| e.last_name as seller,
+	e.first_name||' '||e.last_name as seller,
 	floor(AVG(s.quantity * p.price)) as average_income
 from employees e
 inner join sales s 
 	on s.sales_person_id = e.employee_id 
 inner join products p
 	on p.product_id = s.product_id
-group by e.first_name ||' '|| e.last_name
+group by e.first_name||' '||e.last_name
 having floor(AVG(s.quantity * p.price)) < (select total_average_income from total_avg_amount)
 order by floor(AVG(s.quantity * p.price)) asc;
 
@@ -41,7 +41,7 @@ order by floor(AVG(s.quantity * p.price)) asc;
 with dow_amount as 
 (
 select 
-	e.first_name ||' '|| e.last_name as seller,
+	e.first_name||' '||e.last_name as seller,
 	extract (isodow from s.sale_date) as day_number,
 	to_char(s.sale_date, 'day') as day_of_week,
 	floor(SUM(s.quantity * p.price)) as income
@@ -50,8 +50,8 @@ inner join sales s
 	on s.sales_person_id = e.employee_id 
 inner join products p
 	on p.product_id = s.product_id
-group by e.first_name ||' '|| e.last_name, to_char(s.sale_date, 'day'), extract (isodow from s.sale_date)
-order by extract (isodow from s.sale_date), e.first_name ||' '|| e.last_name
+group by e.first_name||' '||e.last_name, to_char(s.sale_date, 'day'), extract (isodow from s.sale_date)
+order by extract (isodow from s.sale_date), e.first_name||' '||e.last_name
 )
 -- запрос для вывода необходимой для отчета информации
 select				 
@@ -102,9 +102,9 @@ where p.price = 0
 )
 -- выводим информацию для отчета
 select 				 
-	c.first_name ||' '|| c.last_name as customer,
+	c.first_name||' '||c.last_name as customer,
 	r.sale_date,
-	e.first_name ||' '|| e.last_name as seller
+	e.first_name||' '||e.last_name as seller
 from rn_tab r
 inner join employees e 
 	on e.employee_id = r.sales_person_id
