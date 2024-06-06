007548d06c4baa520cb030f0b5f623e9181bd79c
DAVolgas <volgas1974@gmail.com>
2024-06-06 17:06:51 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -15,18 +15,19 @@ group by e.first_name || ' ' || e.last_name
 order by FLOOR(SUM(s.quantity * p.price)) desc
 limit 10;
 
--- создаем СТЕ с расчетом средней выручки по всем продавцам с округлением в меньшую сторону до целого числа
+-- СТЕ средняя выручка по всем продавцам с округлением
 with total_avg_amount as (
-    select floor(avg(s.quantity * p.price)) as total_average_income
+    select FLOOR(AVG(s.quantity * p.price)) as total_average_income
     from sales as s
     inner join products as p
         on s.product_id = p.product_id
 )
 
--- отчет о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+-- отчет о продавцах, чья средняя выручка за сделку
+-- меньше средней выручки за сделку по всем продавцам
 select
     e.first_name || ' ' || e.last_name as seller,
-    floor(avg(s.quantity * p.price)) as average_income
+    FLOOR(AVG(s.quantity * p.price)) as average_income
 from employees as e
 inner join sales as s
     on e.employee_id = s.sales_person_id
@@ -34,18 +35,18 @@ inner join products as p
     on s.product_id = p.product_id
 group by e.first_name || ' ' || e.last_name
 having
-    floor(avg(s.quantity * p.price))
+    FLOOR(AVG(s.quantity * p.price))
     < (select total_average_income from total_avg_amount)
-order by floor(avg(s.quantity * p.price)) asc;
+order by FLOOR(AVG(s.quantity * p.price)) asc;
 
 
--- в СТЕ выводим номер и название дня недели + имена и фамалии продавцов + суммарная выручка
+-- в СТЕ номер и название дня недели
 with dow_amount as (
     select
         e.first_name || ' ' || e.last_name as seller,
-        extract(isodow from s.sale_date) as day_number,
-        to_char(s.sale_date, 'day') as day_of_week,
-        floor(sum(s.quantity * p.price)) as income
+        EXTRACT(isodow from s.sale_date) as day_number,
+        TO_CHAR(s.sale_date, 'day') as day_of_week,
+        FLOOR(SUM(s.quantity * p.price)) as income
     from employees as e
     inner join sales as s
         on e.employee_id = s.sales_person_id
@@ -53,10 +54,10 @@ with dow_amount as (
         on s.product_id = p.product_id
     group by
         e.first_name || ' ' || e.last_name,
-        to_char(s.sale_date, 'day'),
-        extract(isodow from s.sale_date)
+        TO_CHAR(s.sale_date, 'day'),
+        EXTRACT(isodow from s.sale_date)
     order by
-        extract(isodow from s.sale_date), e.first_name || ' ' || e.last_name
+        EXTRACT(isodow from s.sale_date), e.first_name || ' ' || e.last_name
 )
 
 -- запрос для вывода необходимой для отчета информации
@@ -72,37 +73,37 @@ with age_cat as (
     select
         *,
         case
-            when c.age between 16 and 25 then '16-25'
-            when c.age between 26 and 40 then '26-40'
-            when c.age > 40 then '40+'
+            when age between 16 and 25 then '16-25'
+            when age between 26 and 40 then '26-40'
+            when age > 40 then '40+'
         end as age_category
-    from customers as c
+    from customers
 )
 
 -- считаем количество покупателй по возрастным категориям
 select
     age_category,
-    count(age) as age_count
+    COUNT(age) as age_count
 from age_cat
 group by age_category
 order by age_category;
 
 -- выводим количество покупателй и выручку по месяцам
 select
-    to_char(s.sale_date, 'yyyy-mm') as selling_month,
-    count(distinct s.customer_id) as total_customers,
-    floor(sum(s.quantity * p.price)) as income
+    TO_CHAR(s.sale_date, 'yyyy-mm') as selling_month,
+    COUNT(distinct s.customer_id) as total_customers,
+    FLOOR(SUM(s.quantity * p.price)) as income
 from sales as s
 inner join products as p
     on s.product_id = p.product_id
-group by to_char(s.sale_date, 'yyyy-mm')
-order by to_char(s.sale_date, 'yyyy-mm');
+group by TO_CHAR(s.sale_date, 'yyyy-mm')
+order by TO_CHAR(s.sale_date, 'yyyy-mm');
 
 -- CTE - добавляем нумерацию строк акционных продаж по id покупателя с сортировкой по дате и id покупателя
 with rn_tab as (
     select
         *,
-        row_number()
+        ROW_NUMBER()
             over (
                 partition by s.customer_id order by s.sale_date, s.customer_id
             )
