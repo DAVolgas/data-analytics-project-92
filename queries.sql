96f085de9eb72dbca385a9265330262e67ca5413
DAVolgas <volgas1974@gmail.com>
2024-06-06 12:47:32 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
-select COUNT(customer_id) as customers_count from customers c 
--- запрос выводит общее количество записей в поле customer_id таблицы customers и присваивает полю в результате запроса имя customers_count
--- т.к. customer_id уникальный для каждого покупателя, то в результате запроса выводится общее количество покупателей
+select COUNT(customer_id) as customers_count from customers
+-- запрос выводит общее количество покупателей
 
 select 											-- отчет о десятке лучших продавцов
 	e.first_name ||' '|| e.last_name as seller, -- объединение имени и фамилии в одно поле
